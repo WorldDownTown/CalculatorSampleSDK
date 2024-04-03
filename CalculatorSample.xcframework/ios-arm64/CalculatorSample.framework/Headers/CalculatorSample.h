019129843822eeba0069596d310963f20fa635d5
WorldDownTown <WorldDownTown@gmail.com>
2024-04-03 09:53:07 +0900

    CalculatorSample.xcframework

---
+++ CalculatorSample.xcframework/ios-arm64/CalculatorSample.framework/Headers/CalculatorSample.h
@@ -0,0 +1,18 @@
+//
+//  CalculatorSample.h
+//  CalculatorSample
+//
+//  Created by Keisuke Shoji on 2024/04/02.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CalculatorSample.
+FOUNDATION_EXPORT double CalculatorSampleVersionNumber;
+
+//! Project version string for CalculatorSample.
+FOUNDATION_EXPORT const unsigned char CalculatorSampleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CalculatorSample/PublicHeader.h>
+
+
